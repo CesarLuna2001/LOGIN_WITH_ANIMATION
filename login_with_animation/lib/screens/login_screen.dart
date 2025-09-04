b82c81806670023a3cd3c078fc1279afba775627
Cesarluna200115 <cesarluna200115@outlook.com>
2025-09-04 10:20:32 -0600

    Clase: feat(login): add email and password inputs

--- login_with_animation/lib/screens/login_screen.dart
+++ login_with_animation/lib/screens/login_screen.dart
@@ -9,18 +9,62 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Estado para mostrar u ocultar la contraseña
+  bool _isPasswordVisible = false;
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset('assets/animated_login_character.riv')
-            )
-          ],
-        )
-      )
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset('assets/animated_login_character.riv'),
+              ),
+              const SizedBox(height: 10),
+              // Campo de texto del Email
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Campo de texto de Password
+              TextField(
+                obscureText: !_isPasswordVisible, // Cambia según el estado
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  // Icono para mostrar/ocultar contraseña
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
